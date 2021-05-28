b64d2f544264ab8dfa0d2c8e070b5388f6073956
Li Xinyue <lxy_2008focus@126.com>
2021-05-28 11:54:22 +0800

    环境更改提交

--- EP_DataShining.r
+++ EP_DataShining.r
@@ -1,4 +1,4 @@
-data.ep.raw<-as.data.table(read.csv(file="InnerTuning_20210524.csv"))
+data.ep.raw<-as.data.table(read.csv(file="OriginalData/InnerTuning_20210522.csv"))
 
 #不能直接label，因为每次重新执行labView时会重置，因此可能重复
 data.ep.raw$timeLabel<-format(data.ep.raw$Time,format="%Y-%m-%d %H:%M:%S")
@@ -11,8 +11,8 @@ data.ep.roomResponse.second<-cbind(data.ep.raw[,.(Time=Time[1],ID=ID[1],Label=La
                       data.ep.raw[,lapply(.SD,mean,na.rm=TRUE),
                                    .SDcols=c("Flowrate","Totalpressure","Subpressure","InWaterT","OutWaterT",
                                                    "InWindT","OutWindT","Valveopening","Vset","Fset","Tset",       
-                                                   "Powerset","Fre","HeatingRate",
-                                             "t_out_set","t_return_set"),by=timeLabel][,-"timeLabel"])%>%
+                                             "t_out_set","t_return_set",
+                                             "Powerset","Fre","HeatingRate"),by=timeLabel][,-"timeLabel"])%>%
                 mutate(.,Time=as.POSIXct(.$Time))%>%as.data.table()
 
 
@@ -20,7 +20,9 @@ data.ep.roomResponse.second<-cbind(data.ep.raw[,.(Time=Time[1],ID=ID[1],Label=La
 ####根据时间分配TestId####
 data.ep.roomResponse.second$testId<-"prepare"
 
-info.ep.testId<-read.xlsx(file="Info_TestId.xlsx",sheetName = "0525")%>%as.data.table(.)
+info.ep.testId<-read.xlsx(file="Info_TestId.xlsx",sheetName = "0522")%>%as.data.table(.)
+
+
 apply(info.ep.testId[,c("start","end","testId")], MARGIN = 1, function(x){
   data.ep.roomResponse.second[Time %within% interval(start=as.POSIXct(x[1]),end=as.POSIXct(x[2]))]$testId<<-as.character(x[3])
   })
@@ -29,28 +31,35 @@ data.ep.roomResponse.second<-merge(x=data.ep.roomResponse.second,
 
 
 
-data.ep.roomResponse.second[testId=="K2_2"]%>%ggplot(data=.,aes(x=Time,y=InWindT))+geom_line()
+data.ep.roomResponse.second[testId=="OrgCascWithSetpoint"]%>%ggplot(data=.,aes(x=Time,y=InWindT))+geom_line()
 
-data.ep.roomResponse.second[!is.na(Kp),
-                            c("Label","testId","Time","Valveopening","Vset","Flowrate","OutWindT","InWindT","t_out_set","Kp","Ti")]%>%
+data.ep.roomResponse.second[!is.na(Kp)&Kp!=0,
+                            c("Label","testId","Time","Valveopening","Vset","Flowrate","OutWindT","InWindT","t_out_set","t_return_set","Kp","Ti")]%>%
   mutate(.,para=paste("Kp"=Kp,"Ti"=Ti,sep=","))%>%
   melt(.,id.var=c("Label","testId","Time","Ti","para"))%>%
   as.data.table(.)%>%{
-    ggplot(data=.[variable %in% c("Valveopening","Vset")],aes(x=Label,y=value,color=variable,lty=variable,width=2,group=variable))+
+    ggplot(data=.[variable %in% c("Valveopening","Vset")],aes(x=Label,y=value,color=variable,lty=variable,width=4,group=variable))+
       geom_line()+
-      geom_line(data=.[variable %in% c("OutWindT","InWindT","t_out_set")],aes(x=Label,y=(value-20)/0.4))+
-      scale_y_continuous(sec.axis = sec_axis(~.*0.4+20,name = "Temperature"))+
+      geom_line(data=.[variable %in% c("OutWindT","InWindT","t_out_set","t_return_set")],aes(x=Label,y=value))+#(value-20)/0.4))+
+      #scale_y_continuous(sec.axis = sec_axis(~.*0.4+20,name = "Temperature"))+
       facet_wrap(~para+testId,nrow = 2)+
-      theme_bw()+theme(axis.text=element_text(size=18),axis.title=element_text(size=18,face="bold"),
-                       legend.text = element_text(size=16),legend.position = c(0.85,0.2))#
+      theme_bw()+theme(axis.text=element_text(size=18),axis.title=element_text(size=18,face="bold"),#legend.position = c(0.85,0.2),
+                       legend.text = element_text(size=16))#
     }
   
+##查看最后2min变化
+data.ep.roomResponse.second[testId=="OrgCascWithSetpoint"]%>%
+  .[(nrow(nn)-120):nrow(nn),c("Time","OutWindT","t_return_set")]%>%#,"t_out_set""OutWindT",
+  melt(data=.,id.var=c("Time"))%>%
+  ggplot(data=.,aes(x=variable,y=value,color=variable))+geom_boxplot()+ylim(25,30)
+
+
 #输出秒级数据
 write.xlsx(data.ep.roomResponse.second,file="SecVer_RoomResponse_20210507.xlsx")
 
 ####看一下阀门的开关####
 #sheetName = "阀门开关响应"
-data.ep.valveFlow<-as.data.table(read.csv(file="SecVer_ResponseOnOffTest_20210506.csv"))%>%
+data.ep.valveFlow<-as.data.table(read.csv(file="OriginalData/SecVer_ResponseOnOffTest_20210506.csv"))%>%
   mutate(.,TestId=as.factor(.$TestId),isOn=(.$from<.$to))%>%as.data.table(.)%>%.[complete.cases(.)]
 
 ggplot(data=data.ep.valveFlow[!TestId%in%c("T4","RTR_1")],aes(x=Valveopening,y=Flowrate,color=isOn,lty=TestId))+geom_line()+facet_wrap(~TestId,nrow = 2)+
